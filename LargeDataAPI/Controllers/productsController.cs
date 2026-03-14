ee26b04ffa02c8cb4485bd84fa8bae194431ec09
SultanMohammad29 <Hackathon46@USHYDHACKATH46>
2026-03-14 12:52:41 +0530

    test

---
+++ LargeDataAPI/Controllers/productsController.cs
@@ -0,0 +1,37 @@
+﻿using LargeDataAPI.DBContext;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LargeDataAPI.Controllers
+{
+    public class productsController : Controller
+    {
+        private readonly AppDBContext _context;
+
+        public productsController(AppDBContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+        //Pagination for large data
+        [HttpGet]
+        public IActionResult GetProducts(int page = 1, int pageSize = 20)
+        {
+            var data = _context.Products.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+            return Ok(data);
+        }
+        //Filtering for large data
+        public IActionResult GetProductsByCategory(string category)
+        {
+            var data = _context.Products.Where(p => p.Category == category).ToList();
+            return Ok(data);
+        }
+    }
+}
