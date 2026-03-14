6fda1685b7385d1728e168d9f035ead33d62ccc4
Hackathon46 <Hackathon46@USHYDHACKATH46>
2026-03-14 11:20:52 +0530

    Add project files.

---
+++ LargeDataAPI/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LargeDataAPI.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
